2fad4abb823e0230d2cb4cf44b9450f2dc1c1bc1
yourigruiters <i-youri-i@hotmail.com>
2021-05-15 19:38:33 +0200

    Add default styles

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
-import styled from '@emotion/styled';
 import { ColorTheme, ThemeType } from './styles/theme';
 import HomePage from './containers/pages/home';
-
-const GlobalStyles = styled.div`
-  * {
-    transition: background 0.25s linear;
-  }
-`;
 
 interface Props {
   reduxTheme: ThemeType;
@@ -18,13 +11,11 @@ interface Props {
 const App: React.FC<Props> = ({ reduxTheme }) => {
   return (
     <ThemeProvider theme={ColorTheme[reduxTheme]}>
-      <GlobalStyles>
-        <Switch>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </GlobalStyles>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
     </ThemeProvider>
   );
 };
