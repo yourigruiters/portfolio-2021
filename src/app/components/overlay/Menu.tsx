95b0efcc9d016f0d409144442aafc6ca91dcc0de
yourigruiters <i-youri-i@hotmail.com>
2021-05-16 18:24:14 +0200

    Cleanup MenuButton

--- src/app/components/overlay/Menu.tsx
+++ src/app/components/overlay/Menu.tsx
@@ -1,77 +1,29 @@
 import styled from '@emotion/styled';
 import React from 'react';
 
-const Container = styled.div`
-  #webapp_cover {
-    width: 40px;
-  }
+const Container = styled.div``;
 
-  #menu_button {
-    width: 40px;
-    overflow: hidden;
-  }
+const Cover = styled.div`
+  width: 40px;
+`;
 
-  #menu_checkbox {
-    display: none;
-  }
+const Button = styled.div`
+  width: 40px;
+  overflow: hidden;
+`;
 
-  #menu_label {
-    position: relative;
-    display: block;
-    height: 29px;
-    cursor: pointer;
-  }
+const Input = styled.input`
+  display: none;
 
-  #menu_label:before,
-  #menu_label:after,
-  #menu_text_bar {
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 5px;
-    background-color: #fff;
-  }
-
-  #menu_label:before,
-  #menu_label:after {
-    content: '';
-    transition: 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55) left;
-  }
-
-  #menu_label:before {
-    top: 0;
-  }
-
-  #menu_label:after {
-    top: 12px;
-  }
-
-  #menu_text_bar {
-    top: 24px;
-  }
-
-  #menu_text_bar:before {
-    content: 'MENU';
-    position: absolute;
-    top: 5px;
-    right: 0;
-    left: 0;
-    margin-top: -5px;
-    color: #fff;
-    font-size: 12px;
-    font-weight: bold;
-    text-align: center;
-  }
-
-  #menu_checkbox:checked + #menu_label:before {
+  &:checked + .menu_label:before {
     left: -40px;
   }
 
-  #menu_checkbox:checked + #menu_label:after {
+  &:checked + .menu_label:after {
     left: 40px;
   }
 
-  #menu_checkbox:checked + #menu_label #menu_text_bar:before {
+  &:checked + .menu_label .menu_text_bar:before {
     animation: moveUpThenDown 0.8s ease 0.2s forwards,
       shakeWhileMovingUp 0.8s ease 0.2s forwards,
       shakeWhileMovingDown 0.2s ease 0.8s forwards;
@@ -123,19 +75,67 @@ const Container = styled.div`
   }
 `;
 
+const Label = styled.label`
+  position: relative;
+  display: block;
+  height: 29px;
+  cursor: pointer;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 5px;
+    background-color: #fff;
+    transition: 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55) left;
+  }
+
+  &:before {
+    top: 0;
+  }
+
+  &:after {
+    top: 12px;
+  }
+`;
+
+const MenuText = styled.div`
+  position: absolute;
+  top: 24px;
+  left: 0;
+  width: 100%;
+  height: 5px;
+  background-color: #fff;
+
+  &:before {
+    content: 'MENU';
+    position: absolute;
+    top: 5px;
+    right: 0;
+    left: 0;
+    margin-top: -5px;
+    color: #fff;
+    font-size: 12px;
+    font-weight: bold;
+    text-align: center;
+  }
+`;
+
 interface Props {}
 
 const Menu: React.FC<Props> = () => {
   return (
     <Container>
-      <div id="webapp_cover">
-        <div id="menu_button">
-          <input type="checkbox" id="menu_checkbox" />
-          <label htmlFor="menu_checkbox" id="menu_label">
-            <div id="menu_text_bar"></div>
-          </label>
-        </div>
-      </div>
+      <Cover>
+        <Button>
+          <Input type="checkbox" id="menu_checkbox" />
+          <Label htmlFor="menu_checkbox" className="menu_label">
+            <MenuText className="menu_text_bar"></MenuText>
+          </Label>
+        </Button>
+      </Cover>
     </Container>
   );
 };
